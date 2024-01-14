5418bd9ae6fec4437196c17d37f3ddadad2a6f28
Ariel-Vargas <arielvargas2004@hotmail.com>
2024-01-14 14:10:45 -0500

    Class new

--- proyecto/src/main/java/com/espol/tictactoe/controller/GamePlay.java
+++ proyecto/src/main/java/com/espol/tictactoe/controller/GamePlay.java
@@ -11,10 +11,6 @@ import javafx.scene.layout.VBox;
 import com.espol.tictactoe.App;
 import com.espol.tictactoe.model.GameData;
 import com.espol.tictactoe.state.GamePlayContext;
-import java.io.FileOutputStream;
-import java.io.ObjectOutputStream;
-import javafx.event.EventHandler;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 
 /**
@@ -41,7 +37,7 @@ public class GamePlay {
 
     private GameData gameData;
     @FXML
-    private GridPane gameMatrix;
+    public static GridPane gameMatrix;
 
     public void initialize() {
         this.gameData = GamePlayContext.getInstance().getGameData();
@@ -58,24 +54,5 @@ public class GamePlay {
         }
     }
     
-    private void savePlay(){
-        saveGame.setOnMouseClicked(new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent event) {
-                StringBuilder text = new StringBuilder(); 
-
-                gameMatrix.getChildren().forEach((child) -> {
-                    text.append(child.toString()).append(" ");
-                });
-
-                try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("game_data.ser"))) {
-                    oos.writeObject(text.toString());
-                    System.out.println("Datos del juego guardados correctamente.");
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-    }
 
 }
