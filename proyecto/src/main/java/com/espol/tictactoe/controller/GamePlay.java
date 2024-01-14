5053206202c329127283d166084b073bdedf7f3f
Braulio Rivas Abad <brauliorivas2004@gmail.com>
2024-01-14 13:52:16 -0500

    basic controls: set X and O

--- proyecto/src/main/java/com/espol/tictactoe/controller/GamePlay.java
+++ proyecto/src/main/java/com/espol/tictactoe/controller/GamePlay.java
@@ -1,11 +1,20 @@
 
 package com.espol.tictactoe.controller;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.util.Objects;
 
+import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 
 import com.espol.tictactoe.App;
@@ -29,16 +38,19 @@ public class GamePlay {
     private Label playerTwo;
 
     @FXML
-    private VBox returnButton;
+    private Button saveGame;
 
     @FXML
-    private Button saveGame;
+    private GridPane board;
 
     private GameData gameData;
 
     public void initialize() {
         this.gameData = GamePlayContext.getInstance().getGameData();
-        System.out.println(gameData);
+        gameMode.setText(gameData.getGameMode().toString());
+        playerOne.setText(gameData.getPlayerOne().getName());
+        playerTwo.setText(gameData.getPlayerTwo().getName());
+
     }
 
     @FXML
@@ -51,4 +63,45 @@ public class GamePlay {
         }
     }
 
+    private void setX(int row, int col) {
+        setImage(row, col, "file:src/main/resources/com/espol/tictactoe/img/cross.png");
+    }
+
+    private void setO(int row, int col) {
+        setImage(row, col, "file:src/main/resources/com/espol/tictactoe/img/circle.png");
+    }
+    private void setXonHover(int row, int col) {
+        setImage(row, col, "file:src/main/resources/com/espol/tictactoe/img/greycross.png");
+    }
+
+    private void setOonHover(int row, int col) {
+        setImage(row, col, "file:src/main/resources/com/espol/tictactoe/img/greycircle.png");
+    }
+
+    private void setImage(int row, int col, String imgPath) {
+        VBox cell = getCell(row, col);
+        assert cell != null;
+        ImageView imgView = (ImageView) cell.getChildren().get(0);
+
+        Image img = new Image(imgPath);
+        imgView.setImage(img);
+    }
+
+    private void clearImage(int row, int col) {
+        VBox cell = getCell(row, col);
+        assert cell != null;
+        ImageView img = (ImageView) cell.getChildren().get(0);
+        img.setImage(null);
+    }
+
+    private VBox getCell(Integer row, Integer col) {
+        for (Node node : board.getChildren()) {
+            Integer rowIndex = GridPane.getRowIndex(node) == null ? 0 : GridPane.getRowIndex(node);
+            Integer colIndex = GridPane.getColumnIndex(node) == null ? 0 : GridPane.getColumnIndex(node);
+            if (Objects.equals(rowIndex, row) && Objects.equals(colIndex, col)) {
+                return (VBox) node;
+            }
+        }
+        return null;
+    }
 }
